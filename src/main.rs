37ae717a61d196370d8515e086536750d27e74d7
Marcos <marcoscarlomagno1@gmail.com>
2024-12-30 19:45:03 -0300

    feat: load remote wasm + compare hashes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-use base64::Engine;
+use std::str::FromStr;
 
-fn process_args() -> (String, String, String) {
+use soroban_cli::commands::contract::info::shared::{fetch_wasm, Args};
+use soroban_cli::config::{network, ContractAddress};
+use soroban_cli::print::Print;
+use sha2::{Sha256, Digest};
+
+pub struct CmdArgs {
+  local: String,
+  remote: String,
+}
+
+fn hash_wasm(bytes: &[u8]) -> String {
+  let mut hasher = Sha256::new();
+  hasher.update(bytes);
+  let result = hasher.finalize();
+  format!("{:x}", result)  // converts to hex string
+}
+
+fn process_args() -> CmdArgs {
   let args: Vec<String> = std::env::args().collect();
 
-  let mut network = String::new();
   let mut local = String::new();
   let mut remote = String::new();
 
   let mut i = 1;
   while i < args.len() {
     match args[i].as_str() {
-      "--network" => {
-        if i + 1 < args.len() {
-          network = args[i + 1].clone();
-          i += 2;
-        }
-      }
       "--local" => {
         if i + 1 < args.len() {
           local = args[i + 1].clone();
@@ -32,28 +42,52 @@ fn process_args() -> (String, String, String) {
     }
   }
 
-  (network, local, remote)
+  return CmdArgs { local, remote }
 }
 
 fn load_local_file(path: String) -> Result<String, Box<dyn std::error::Error>> {
   let wasm_bytes = std::fs::read(&path)?;
-  String::from_utf8(wasm_bytes).map_err(|e| e.into())
+  let res = hash_wasm(&wasm_bytes);
+  Ok(res)
 }
 
-// fn load_remote_file(contract_id: String) -> Result<String, std::error::Error> {
-//   // TODO: use stellar sdk to load on-chain wasm code
-// }
- 
-fn main() {
-  let (network, local, remote) = process_args();
+async fn load_remote_file(contract_id: String) -> Result<String, Box<dyn std::error::Error>> {
+  let print = Print::new(true);
+  let contract_id = ContractAddress::from_str(&contract_id).ok();
+  let network_args = network::Args {
+    rpc_url: Some(String::from("https://mainnet.sorobanrpc.com")),
+    network_passphrase: Some(String::from("Public Global Stellar Network ; September 2015")),
+    rpc_headers: vec![
+      (String::from("Content-Type"), String::from("application/json")),
+    ],
+    ..Default::default()
+  };
+  let args = Args {
+    contract_id,
+    network: network_args,
+    ..Default::default()
+  };
+  let (wasm_bytes, _, _) = fetch_wasm(&args, &print).await?;
 
-  if network.is_empty() || local.is_empty() || remote.is_empty() {
+  let res = match wasm_bytes {
+    Some(bytes) => hash_wasm(&bytes),
+    None => panic!("could not load remote contract"),
+  };
+
+  Ok(res)
+}  
+ 
+#[tokio::main]
+async fn main() {
+  let CmdArgs { local, remote } = process_args();
+
+  if local.is_empty() || remote.is_empty() {
     eprintln!("Error: Missing required arguments");
-    eprintln!("Usage: program --network <network> --local <local_path_to_wasm> --remote <contract_address>");
+    eprintln!("Usage: program --local <local_path_to_wasm> --remote <contract_address>");
     std::process::exit(1);
   }
 
-  let wasm_string = match load_local_file(local.clone()) {
+  let local_wasm_hash = match load_local_file(local.clone()) {
     Ok(content) => content,
     Err(e) => {
       eprintln!("Error reading WASM file: {}", e);
@@ -61,7 +95,49 @@ fn main() {
     }
   };
 
-  println!("Network: {}", network);
-  println!("Local path: {}", local);
-  println!("Remote: {}", remote);
+  let remote_wasm_hash = match load_remote_file(remote.clone()).await {
+    Ok(wasm) => wasm,
+    Err(e) => {
+        eprintln!("Error fetching remote WASM: {}", e);
+        std::process::exit(1);
+    }
+  };
+
+  if local_wasm_hash == remote_wasm_hash {
+    println!("✅ WASM files match!");
+  } else {
+      eprintln!("❌ WASM files do not match!");
+      std::process::exit(1);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use tokio;
+
+  #[tokio::test]
+  async fn test_compare_remote_vs_local_contracts() {
+    let local = String::from("./fixture/test.wasm");
+    let remote = String::from("CB5HA53QWBLOCD7LQOFZ4FIOSQS2ZUA7KIBZYOV6D4CPJWXIYGX2OBAC");
+
+    let local_wasm_hash = match load_local_file(local.clone()) {
+        Ok(content) => content,
+        Err(e) => {
+            panic!("Error reading WASM file: {}", e);
+        }
+    };
+
+    let remote_wasm_hash = match load_remote_file(remote.clone()).await {
+        Ok(wasm) => wasm,
+        Err(e) => {
+            panic!("Error fetching remote WASM: {}", e);
+        }
+    };
+
+    assert_eq!(local_wasm_hash, remote_wasm_hash, 
+        "WASM hashes don't match!\nLocal: {}\nRemote: {}", 
+        local_wasm_hash, remote_wasm_hash
+    );
+  }
 }
